ba174f90562e12fff972ff9a05d5c45a22291841
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2021-08-22 22:09:29 -0300

    .

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show__menu");
+  });
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show__menu");
+  });
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  // When we click on each nav__link, we remove the show-menu class
+  navMenu.classList.remove("show__menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName("skills__content"),
+  subSkillsContent = document.getElementsByClassName("subskills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header"),
+  subSkillsHeader = document.querySelectorAll(".subskills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
+
+function toggleSubSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < subSkillsContent.length; i++) {
+    subSkillsContent[i].className = "subskills__content subskills__close";
+  }
+
+  if (itemClass === "subskills__content subskills__close") {
+    this.parentNode.className = "subskills__content subskills__open";
+  }
+}
+
+subSkillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSubSkills);
+});
+
+/*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+/*==================== TESTIMONIAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
